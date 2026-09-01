ffce8eddd0a2287706fa39f9fa859ff243668544
ralex0111-sudo <ralex0111@gmail.com>
2026-09-01 17:06:51 +0530

    Add files via upload

---
+++ Task day2.js
@@ -0,0 +1,252 @@
+//Print all three values.
+//var sname="Alex";
+//let sage=27;
+//const collegename="Francis Xavier College";
+//console.log(sname);
+//console.log(sage);
+//console.log(collegename);
+
+//Change the var value.
+//sname="Ramesh";
+
+//Change the let value.
+//sage=55
+
+//Try changing the const value and observe the error.
+//collegename="St. Joseph College";
+
+//Try redeclaring the var variable.
+//var sname="jacky";
+
+//Try redeclaring the let variable and observe the error.
+//let sage=56
+
+//Task2
+//var name=prompt("Enter your name");
+//var age=prompt("Enter your age");
+//var city=prompt("Enter your city name");
+//var college=prompt("Enter your college name");
+
+
+// Task 3 — Welcome Message
+//var username=prompt("Enter your name");
+//alert("Welcome Naveen!");
+
+//Task 4 — Age Calculator
+//var dob=prompt("Enter your date of birth");
+//var currentYear=2024;
+//var birthyear=1999;
+//var age=currentYear-birthyear;
+//console.log(birthyear);
+//console.log(age);
+
+/*Task 5 — Identify Data Types
+var a="Hello";
+console.log(typeof (a));
+var num=100
+console.log(typeof (num));
+var num1=25.5
+console.log(typeof (num1));
+var boo=true;
+console.log(typeof (boo));
+var boo1=false;
+console.log(typeof (boo1));
+let und
+console.log(typeof (und));
+var n=null;
+console.log(typeof (n));
+*/
+
+/*Task 6 — Student Data
+var student={
+    name:"Alex",
+    age:27,
+    city:"Delhi",
+    qualification:"Mca",
+    isStudent:true,
+}
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.qualification);
+console.log(student.isStudent);
+*/
+
+/*Task 7 — Fruit Array
+var furits=["Apple","Mango","orange","Banana","Grapes","pappaya"];
+console.log(furits);
+console.log(furits[1]);
+console.log(furits[furits.length - 1]);
+console.log(furits.length)*/
+
+/*Arithmetic Operator Tasks
+Task 8 — Basic Calculator
+let a = 20;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+*/
+
+/*Task 9 — Shopping Bill
+var shirt= 999
+var Pant = 1499
+var Shoes = 1999
+var Total=shirt+Pant+Shoes
+console.log(Total);
+*/
+
+/*Task 10 — Simple Marks Calculation
+var Tamil = 80
+var English = 75
+var Maths = 90
+var totalMarks = Tamil + English + Maths
+console.log(totalMarks);
+var average=(totalMarks/300);
+console.log(average);
+*/
+
+
+/*ex output 
+10
+11
+let a = 10;
+let b = a++;
+console.log(a);
+console.log(b);
+*/
+
+/*ex out 
+11
+11
+variable side we have a and value is 10 so it meets the ++ before the value side a so it gets inremeanted
+let a = 10;
+let b = ++a;
+console.log(a);
+console.log(b);
+*/
+
+/*19
+20
+let a = 20;
+let b = a--;
+console.log(a);
+console.log(b);*/
+
+/*19
+19
+let a = 20;
+let b = --a;
+console.log(a);
+console.log(b);*/
+
+/*Task 15 — Find the Final Values
+let a = 5;
+let b = a++;
+let c = ++a;
+let d = b--;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+*/
+
+/*Task 16 — Assignment Operators
+let num = 10;
+num += 5;
+console.log(num);
+
+let num2 = 10;
+num2 -= 5;
+console.log(num2);
+
+let num3 = 10;
+num3 *= 5;
+console.log(num3);
+
+let num4 = 10;
+num4 /= 5;
+console.log(num4);
+
+let num5 = 10;
+num5 %= 5;
+console.log(num5)
+
+let num6 = 10;
+num6 **= 3;
+console.log(num6);
+;
+*/
+
+/*Task 17 — Mini Student Profile
+
+var sname="raja";
+var sage=30;
+var scity="salem";
+var scollege="FX"
+var sub=["MCA","BCA","ME","BE","EEE"];
+var student={
+    name:"alex",
+    city:"delhi",
+    age:27,
+    subject:"MCA",
+    isStudent:true,
+}
+console.log(student.name);
+console.log(student.age);
+console.log(student.city);
+console.log(sub[0]);
+console.log(sub[sub.length-1]);
+console.log(sub.length);
+console.log(student);
+*/
+
+/*var aa=prompt("Enter the 1st number");
+var bb=prompt("Enter the 2nd number");
+var tot=Number(aa) + Number(bb);
+console.log(tot);
+*/
+
+
+/*var aa=prompt("Enter the 1st number");
+var bb=prompt("Enter the 2nd number");
+var tot=Number(aa) - Number(bb);
+console.log(tot);*/
+
+/*var aa=prompt("Enter the 1st number");
+var bb=prompt("Enter the 2nd number");
+var tot=Number(aa) / Number(bb);
+console.log(tot);
+*/
+
+/*var aa=prompt("Enter the 1st number");
+var bb=prompt("Enter the 2nd number");
+var tot=Number(aa) * Number(bb);
+console.log(tot);
+*/
+
+/*var aa=prompt("Enter the 1st number");
+var bb=prompt("Enter the 2nd number");
+var tot=Number(aa) % Number(bb);
+console.log(tot);
+*/
+var aa=prompt("Enter the 1st number");
+var bb=prompt("Enter the 2nd number");
+var tot=Number(aa) ** Number(bb);
+console.log(tot);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
